c8dd1f39377dbf33fe8ec9c1e6ee1f4b4b137306
Masaki Hiromoto <masakihiromo@yahoo.co.jp>
2017-12-09 01:06:48 +0900

    色々書いたけどまだ途中

--- z_10_common.c
+++ z_10_common.c
@@ -3,22 +3,56 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// **************************************** タイマ
 static timer_t timer_value = {0, 0, 0};
 static char timer_char[] = {'0', ':', '0', '0', ':', '0', '0' }
 static unsigned int timer_counter = 0;
-
+// **************************************** PID
 static ipd_para_t ipd_para = {
   {0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0}
 };
-
+// **************************************** コース
 static cource_t cource = c_1_hasitemae;
+// **************************************** キャリブ
+static math_features_t black_info = {0 ,0, 0}; //黒色のキャリブ情報
+static math_features_t white_info = {0 ,0, 0}; //白色のキャリブ情報
 
-static math_features_t black_info = {0 ,0, 0};
+////////////////////////////////////////////////////////
+void func_z_10_common_clear_timer(void){
+  timer_value.minute = 0;
+  timer_value.sec = 0;
+  timer_value.millisec = 0;
+  timer_counter = 0;
+  return;
+}
+const timer_t * func_z_10_common_fetch_timer(void){
+  return &timer_value;
+}
+const unsigned int * func_z_10_common_fetch_timer_counter(void){
+  return &timer_counter;
+}
+void func_z_10_common_increment_timer(void){
+  timer_counter++;
+  return;
+}
+void func_z_10_common_increment_timer_counter(void){
+  if(timer_value.millisec_10 <= 98){
+    timer_value.millisec_10++;
+  }else{
+    if(timer_value.sec < 58){
+      timer_value.millisec_10 = 0;
+      timer_value.sec++;
+    }else{
+      if(timer_value.minute < 8){
 
-static math_features_t white_info = {0 ,0, 0};
+      }
+    }
+  }
 
+  return;
+}
 void func_z_10_common_format_timer(timer_t timer){
   itoa(timer_value.minute, &timer_char[0], 1, ' '); //分
   timer_char[1] = ':';
@@ -27,23 +61,6 @@ void func_z_10_common_format_timer(timer_t timer){
   itoa(timer_value.minute, &timer_char[5], 2, '0'); // 10ミリ秒
   return;
 }
-
-void func_z_10_common_clear_timer(void){
-  timer_value.minute = 0;
-  timer_value.sec = 0;
-  timer_value.millisec = 0;
-  timer_counter = 0;
-  return;
-}
-
-const timer_t * func_z_10_common_fetch_timer(void){
-  return &timer_value;
-}
-
-const unsigned int timer_counter * func_z_10_common_fetch_timer_counter(void){
-  return &timer_counter;
-}
-
 bool func_z_10_common_are_same_timer(timer_t * timer_1, timer_t * timer_2){
   if(timer_1->minute != timer_2->minute){
     return false;
--- z_10_common.h
+++ z_10_common.h
@@ -3,12 +3,16 @@
 #define Z_10_COMMON_H
 #include <stdbool.h>
 
+// タイマ，PID，コース，キャリブに関する記述
+
+// タイム
 typedef struct {
   unsigned char minute; //分
   unsigned char sec; //秒
   unsigned char millisec_10; //10ミリ秒
 }timer_t;
 
+// PID
 typedef struct {
   unsigned int integer_10; //10の位
   unsigned int integer_1; //1の位
@@ -23,25 +27,37 @@ typedef struct {
   value_t d; //D
 }ipd_para_t;
 
+// コース
+typdef enum {c_1_hasitemae, c_2_hasiowari, c_3_tonnnerutemae, c_4_tonnneruowari, c_5_goaltemae}cource_t;
+
+// キャリブ
 typedef struct {
   unsigned int min; //最小値
   unsigned int max; //最大値
   double ave; //平均値
 }math_features_t;
 
-typdef enum {c_1_hasitemae, c_2_hasiowari, c_3_tonnnerutemae, c_4_tonnneruowari, c_5_goaltemae}cource_t;
-
-// タイマの数字を文字に変換する関数
-extern void func_z_10_common_format_timer(timer_t timer);
+// **************************************** タイマ
 // タイマの初期化を行う関数
 extern void func_z_10_common_clear_timer(void);
 // タイマの値を取得する関数
 extern const timer_t * func_z_10_common_fetch_timer(void);
 // タイマカウンタの値を取得する関数
-extern const unsigned int timer_counter * func_z_10_common_fetch_timer_counter(void);
+extern const unsigned int * func_z_10_common_fetch_timer_counter(void);
+// タイマの値を10ミリ秒インクリメントする関数
+extern void func_z_10_common_increment_timer(void);
+// タイマカウンタの値を1(10ミリ秒分)インクリメントする関数
+extern void func_z_10_common_increment_timer_counter(void);
+// タイマの数字を文字に変換する関数
+extern void func_z_10_common_format_timer(timer_t timer);
 // 2つのタイマの値が同じか同じでないか判別する関数
 extern bool func_z_10_common_are_same_timer(timer_t * timer_1, timer_t * timer_2);
 // 2つのタイマカウンタの値が同じか同じでないか判別する関数
 extern bool func_z_10_common_are_same_timer_counter(unsigned int * timer_counter_1, unsigned int * timer_counter_2);
+// **************************************** PID
+
+// **************************************** コース
+
+// **************************************** キャリブ
 
 #endif
