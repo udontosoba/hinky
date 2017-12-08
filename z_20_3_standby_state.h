fb22e1bcc75ec8b6dd7eb4fcd1349e1f7836f8aa
Masaki Hiromoto <masakihiromo@yahoo.co.jp>
2017-12-08 22:26:45 +0900

    remove files about standby_state

--- z_20_3_standby_state.h
+++
@@ -1,8 +0,0 @@
-/* z_20_3_standby_state.h */
-#ifndef Z_20_3_STANDBY_STATE_H
-#define Z_20_3_STANDBY_STATE_H
-#include "z_00_2_state.h"
-
-extern State func_z_20_3_standby_state_main(void);
-
-#endif
